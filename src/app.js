3dfb39c90e9ea63744fe0ca29b0f33c070aca22e
JosephRemingston <ljremi@gmail.com>
2025-05-11 22:51:06 +0530

    initial commit

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ connectDB().then(() => {
 app.get("/", (req, res) => {
     res.json("server");
 });
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
